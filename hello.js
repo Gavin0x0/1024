2fd70bafd830b56bbc5e1493b5fe7f8dae269614
Levi_o <632891553@qq.com>
2021-07-14 00:16:19 +0800

    retry

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, helpers } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
 let score = [];
@@ -9,12 +9,26 @@ router.get("/list", (ctx) => {
   target.close();
 });
 
-router.get("/add", () => {
-  score.push({ id: 123, s: 100 });
+router.get("/add", (ctx) => {
+  console.log(helpers.getQuery(ctx));
+  let player = helpers.getQuery(ctx).name;
+  for (let i in score) {
+    console.log(i);
+    if (i == player) {
+      console.log("exist");
+    }
+    score.push(helpers.getQuery(ctx));
+  }
+});
+router.get("/string", (ctx) => {
+  const target = ctx.sendEvents();
+  target.dispatchMessage(JSON.stringify(score));
+  target.close();
 });
 
 router.get("/", (ctx) => {
-  ctx.response.body = "<div></div><div></div><style>body{margin:0;background:#62374e;}div{height:50px;width:50px;margin:50px 0 100px 50px;background:#fdc57b;box-shadow:250px 0 0#fdc57b";
+  ctx.response.body =
+    "<body><div id='app'><input v-model='name' placeholder='name'><button @click='add'>Add</button></div></body><script src='https://unpkg.com/vue@next'></script><script src='https://unpkg.com/axios/dist/axios.min.js'></script><script>    Vue.createApp({  data() {    return {      counter: 0    }  },  methods: {    add() {        axios.get('/add')  .then((r)=>{})  .catch((e)=>{});    }  }}).mount('#app')</script></html>";
   ctx.response.type = "text/html";
 });
 
