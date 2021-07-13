b195ef371d1eda7539e19e3e2d3fe48719fc5b85
Levi_o <632891553@qq.com>
2021-07-13 21:28:16 +0800

    update

--- hello.js
+++ hello.js
@@ -1,17 +1,23 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 const router = new Router();
-let score = []
+let score = [];
 
-router.get("/list",(ctx) => {
+router.get("/list", (ctx) => {
   const target = ctx.sendEvents();
   target.dispatchMessage({ d: score });
   target.close();
 });
 
-router.get("/add",() => {
-  score.push({id: 123,s: 100});
+router.get("/add", () => {
+  score.push({ id: 123, s: 100 });
+});
+
+router.get("/", (ctx) => {
+  ctx.response.body = "<div></div><div></div><style>body{margin:0;background:#62374e;}div{height:50px;width:50px;margin:50px 0 100px 50px;background:#fdc57b;box-shadow:250px 0 0#fdc57b";
+  ctx.response.type = "text/html";
 });
 
 app.use(router.routes());
-addEventListener("fetch", app.fetchEventHandler());
+await app.listen(":80");
+//addEventListener("fetch", app.fetchEventHandler());
